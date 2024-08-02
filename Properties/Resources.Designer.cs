d979b74217d1b3c446dc87178f2242bdb2980cb3
Phlox <168133165+yophlox@users.noreply.github.com>
2024-08-02 18:02:19 -0400

    rename shit

--- Properties/Resources.Designer.cs
+++ Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace YA4KRGDLTool.Properties {
+namespace Moon4KDLTool.Properties {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace YA4KRGDLTool.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("YA4KRGDLTool.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Moon4KDLTool.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
